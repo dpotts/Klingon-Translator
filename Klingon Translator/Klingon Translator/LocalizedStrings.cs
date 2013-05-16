a0aad2680098faabf2d508efd888e0c047c355d3
Daniel Potts <dan.t.potts@gmail.com>
2013-05-16 08:11:43 -0700

    Project Setup

---
+++ Klingon Translator/Klingon Translator/LocalizedStrings.cs
@@ -0,0 +1,12 @@
+﻿using Klingon_Translator.Resources;
+
+namespace Klingon_Translator {
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
