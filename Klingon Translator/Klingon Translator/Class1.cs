db7c02fc1987205a32001fdcc8d86fe6ed8c4767
Daniel Potts <dan.t.potts@gmail.com>
2013-05-17 05:46:17 -0700

    Followed This Tutorial: http://blogs.msdn.com/b/translation/p/windowsphone8.aspx

---
+++ Klingon Translator/Klingon Translator/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klingon_Translator {
+    public class AdmAccessToken {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public string expires_in { get; set; }
+        public string scope { get; set; }
+    }
+}
